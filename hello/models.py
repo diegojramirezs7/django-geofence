15388db2faf69757d3f4d3a28e7e332113272884
Diego Ramirez <diegoramirez@Diegos-MacBook-Pro.local>
2021-01-13 23:49:32 -0800

    added geofenceEvent model

--- hello/models.py
+++ hello/models.py
@@ -3,3 +3,10 @@ from django.db import models
 # Create your models here.
 class Greeting(models.Model):
     when = models.DateTimeField("date created", auto_now_add=True)
+
+class GeofenceEvent(models.Model):
+	time = models.DateTimeField("recorded", auto_now_add=True)
+	event = models.CharField(max_length=255, blank=True)
+	user = models.CharField(max_length=255, blank=True, null=True)
+	project = models.CharField(max_length=255, blank=True, null=True)
+
